6b16e564c9fdb0f340766034e4b385662a9e7445
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-15 21:57:08 +0600

    Update transaction detail model

--- app/models/transaction_detail.rb
+++ app/models/transaction_detail.rb
@@ -1,5 +1,5 @@
 class TransactionDetail < ApplicationRecord
-  belongs_to :user
-  has_many :category_details
+  belongs_to :user, foreign_key: 'author_id'
+  has_many :category_details, dependent: :destroy
   has_many :categories, through: :category_details
 end
