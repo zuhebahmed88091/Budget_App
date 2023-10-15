fd2721cc114d2b35efa863f5bf95a3aee382b85b
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-15 14:58:44 +0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_15_083112) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_15_084346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_15_083112) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "transaction_details", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transaction_details_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
@@ -31,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_15_083112) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "transaction_details", "users"
 end
