87cc7e78f2fa3ab3fb71dd957e65420bf9555b82
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-15 21:58:00 +0600

    Update user model with author id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :transaction_details, dependent: :destroy
+  has_many :transaction_details, dependent: :destroy, foreign_key: 'author_id'
   has_many :categories, dependent: :destroy
 end
