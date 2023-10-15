629b6c394a5e035210d7643f796c6b78f2642452
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-15 15:10:08 +0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_15_090110) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_15_090842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.integer "amount"
+    t.float "amount"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
