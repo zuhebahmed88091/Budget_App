4624d9e597beb7d23701b7d4fc414366f860e9ae
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-15 21:19:30 +0600

    Update transaction details

--- app/models/transaction_detail.rb
+++ app/models/transaction_detail.rb
@@ -1,5 +1,5 @@
 class TransactionDetail < ApplicationRecord
   belongs_to :user
-  has_many :category_details, dependent: :destroy
+  has_many :category_details
   has_many :categories, through: :category_details
 end
