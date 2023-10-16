341c0d13d979f362e3d92f7e0808ad03e8fc3bcc
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-16 15:00:19 +0600

    Fix linters

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,24 @@
 class CategoriesController < ApplicationController
-    def index
-        @categories = current_user.categories.includes(:transaction_details)
-    end
+  def index
+    @categories = current_user.categories.includes(:transaction_details)
+  end
 
-    def new
-        @category = Category.new
-    end
+  def new
+    @category = Category.new
+  end
 
-    def create
-        @category = current_user.categories.build(category_params)
-        if @category.save
-            redirect_to categories_path
-        else
-            render :new
-        end
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def category_params
-        params.require(:category).permit(:name, :icon, :amount)
-    end
+  def category_params
+    params.require(:category).permit(:name, :icon, :amount)
+  end
 end
