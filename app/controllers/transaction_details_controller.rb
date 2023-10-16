4cbfcdeeef081e4c789c45ea2a41f0d4bc345e76
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-16 14:56:42 +0600

    Add Transaction controller

---
+++ app/controllers/transaction_details_controller.rb
@@ -0,0 +1,35 @@
+class TransactionDetailsController < ApplicationController
+    def index
+        if params[:category_id]
+            @category = Category.find(params[:category_id])
+            @updated_transaction_details = @category.transaction_details.where(author_id: current_user)
+            @total_amount = @updated_transaction_details.sum(:amount)
+            @transaction_details = @updated_transaction_details.to_a
+        elsif params[:user_id]
+            @user = User.find(params[:user_id])
+            @transaction_details = @user.transaction_details
+        end
+    end
+
+    def new
+        @transaction_detail = TransactionDetail.new
+        @category = Category.find(params[:category_id])
+    end
+
+    def create
+        @category = current_user.categories.find(params[:category_id])
+        @transaction_detail = current_user.transaction_details.build(transaction_detail_params)
+        @category.transaction_details << @transaction_detail
+        if @category.save
+            redirect_to category_transaction_details_path(@category)
+        else
+            render 'new'
+        end
+    end
+      
+      private
+      
+      def transaction_detail_params
+        params.require(:transaction_detail).permit(:name, :amount, :category_id, :author_id)
+      end     
+end
