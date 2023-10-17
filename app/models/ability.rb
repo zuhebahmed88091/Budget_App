f21e433ba320b3613f92409395d4ff3a6508acdf
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-17 09:38:57 +0600

    Add ability

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.admin?
+      can :manage, :all
+    else
+
+      can :read, [Category, TransactionDetail]
+      can :create, [Category, TransactionDetail]
+    end
+  end
+end
