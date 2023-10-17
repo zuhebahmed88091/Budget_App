b99e5ecc2966f346ea48917696afaa84e37b2b6e
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-17 18:59:23 +0600

    Update transaction model

--- app/models/transaction_detail.rb
+++ app/models/transaction_detail.rb
@@ -5,4 +5,5 @@ class TransactionDetail < ApplicationRecord
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :user, presence: true
 end
